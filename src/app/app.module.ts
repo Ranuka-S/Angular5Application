e55f5566a04cfba6f98112fe0b419cf40980426d
Ranuka-S <ranukasandaruwan@yahoo.com>
2018-02-21 16:06:48 +1100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CustomerComponent } from './components/customer/customer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
